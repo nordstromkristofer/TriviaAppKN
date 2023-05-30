4efe78ca0838c9dd43163b1cbd9b6699d39cac18
nordstromkristofer <nordstromkristofer@gmail.com>
2023-05-30 19:50:09 +0200

    basic form added + home routing

--- triviaApp/src/app/app.module.ts
+++ triviaApp/src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuizFormComponent } from './pages/quiz-form/quiz-form.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuizFormComponent,
+    HomeComponent
   ],
   imports: [
+    FormsModule,
+    MatCardModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NoopAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ triviaApp/src/app/pages/quiz-form/quiz-form.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-quiz-form',
+  templateUrl: './quiz-form.component.html',
+  styleUrls: ['./quiz-form.component.css']
+})
+export class QuizFormComponent implements OnInit {
+
+  formData: any = {};
+
+  onSubmit() {
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
