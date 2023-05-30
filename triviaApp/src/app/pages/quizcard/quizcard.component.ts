c7763c9924d6f16b2082487e717e96cd2fe9df36
nordstromkristofer <nordstromkristofer@gmail.com>
2023-05-31 01:32:07 +0200

    quizcard generated

---
+++ triviaApp/src/app/pages/quizcard/quizcard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-quizard',
+  templateUrl: './quizard.component.html',
+  styleUrls: ['./quizard.component.css']
+})
+export class QuizardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
