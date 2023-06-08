779c4e5b2ed038c9c9eb768d407b8548868bb53e
nordstromkristofer <nordstromkristofer@gmail.com>
2023-06-08 22:58:50 +0200

    shuffle works

--- triviaApp/src/app/pages/quizcard/quizcard.component.ts
+++ triviaApp/src/app/pages/quizcard/quizcard.component.ts
@@ -13,10 +13,9 @@ export class QuizCardComponent implements OnInit {
   quizQuestions: QuizQuestion[] = [];
   currentIndex: number = 0;
   currentQuestion: QuizQuestion | undefined;
+  shuffledAnswers: string[] = [];
   selectedAnswer: string | undefined;
   userAnswers: (string | undefined)[] = [];
-  correctAnswersCount: number = 0;
-  incorrectAnswersCount: number = 0;
 
   constructor(
     private route: ActivatedRoute,
@@ -32,11 +31,30 @@ export class QuizCardComponent implements OnInit {
       this.currentIndex = state?.currentIndex || 0;
 
       this.currentQuestion = this.quizQuestions[this.currentIndex];
+      this.shuffleAnswers();
 
       console.log('Quiz Questions:', this.quizQuestions);
       console.log('Current Index:', this.currentIndex);
       console.log('Current Question:', this.currentQuestion);
+      console.log('Shuffled Answers:', this.shuffledAnswers);
     });
+  }
+
+  shuffleAnswers(): void {
+    if (this.currentQuestion) {
+      const answers = [this.currentQuestion.correct_answer, ...this.currentQuestion.incorrect_answers];
+      this.shuffledAnswers = this.shuffleArray(answers.filter(answer => answer !== undefined));
+    } else {
+      this.shuffledAnswers = [];
+    }
+  }
+
+  shuffleArray(array: any[]): any[] {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
   }
 
   selectAnswer(answer: string | undefined): void {
@@ -59,43 +77,24 @@ export class QuizCardComponent implements OnInit {
     if (this.currentIndex < this.quizQuestions.length - 1) {
       // Store user answer
       this.userAnswers[this.currentIndex] = this.selectedAnswer;
-      console.log('User Answers:', this.userAnswers); // Log user answers
+      console.log('User Answers:', this.userAnswers);
       // Go to the next question
       this.currentIndex++;
       this.currentQuestion = this.quizQuestions[this.currentIndex];
+      this.shuffleAnswers();
       this.selectedAnswer = undefined;
     } else {
       // End of quiz
       console.log('End of quiz');
       // Store user answer for the last question
       this.userAnswers[this.currentIndex] = this.selectedAnswer;
-      console.log('User Answers:', this.userAnswers); // Log user answers
-      // Count the results
-      this.countResults();
+      console.log('User Answers:', this.userAnswers);
       // Navigate to the result page
       this.router.navigate(['/result'], {
         state: {
           userAnswers: this.userAnswers,
-          correctAnswersCount: this.correctAnswersCount,
-          incorrectAnswersCount: this.incorrectAnswersCount
-        }
+        },
       });
     }
-  }
-
-  countResults(): void {
-    this.correctAnswersCount = 0;
-    this.incorrectAnswersCount = 0;
-    for (let i = 0; i < this.quizQuestions.length; i++) {
-      const question = this.quizQuestions[i];
-      const userAnswer = this.userAnswers[i];
-      if (userAnswer === question.correct_answer) {
-        this.correctAnswersCount++;
-      } else {
-        this.incorrectAnswersCount++;
-      }
-    }
-    console.log('Correct Answers:', this.correctAnswersCount);
-    console.log('Incorrect Answers:', this.incorrectAnswersCount);
   }
 }
