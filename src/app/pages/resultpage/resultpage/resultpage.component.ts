65a5426c10f2c3e999ea4b4d8f074213129c48d9
nordstromkristofer <nordstromkristofer@gmail.com>
2023-06-12 12:48:36 +0200

    lost resulpage fix

--- src/app/pages/resultpage/resultpage/resultpage.component.ts
+++ src/app/pages/resultpage/resultpage/resultpage.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
-  selector: 'app-result',
+  selector: 'app-resultpage',
   templateUrl: './resultpage.component.html',
   styleUrls: ['./resultpage.component.css']
 })
